25080bcdd2df14d6682520be6900b0a4a916b287
AseelaAbdulSattar <adeela.abdul.sattar@qbatch.com>
2020-02-11 15:29:05 +0500

    Devise integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  private
+  def record_not_found
+    render plain: "404 Not Found", status: 404
+  end
 end
